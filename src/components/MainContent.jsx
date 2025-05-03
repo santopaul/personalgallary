bba1fd4b4e28cfec50248231143edb0e26db95ea
Santo Paul <santopaul2005@gmail.com>
2025-05-03 16:54:49 -0400

    Version2

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -142,7 +142,7 @@ export default function MainContent() {
                         >
                             {/* If you want to show your pics then you can save your pics in public/images folder like this 1.jpg, 2.jpg, 3.jpg then replace the src to this - /images/${i}.jpg */}
                             <Image
-                                src={`https://images.pexels.com/photos/1759823/pexels-photo-1759823.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
+                                src={`/images/${i}.jpg`}
                                 alt={`Gallery image ${i}`}
                                 width={330}
                                 height={270}
@@ -255,7 +255,7 @@ export default function MainContent() {
                     >
                         {/* When showing your own pics replace image src with this - /images/${selectedImage}.jpg */}
                         <Image
-                            src={`https://images.pexels.com/photos/1759823/pexels-photo-1759823.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
+                            src={`/images/${selectedImage}.jpg`}
                             alt={`Gallery image ${selectedImage}`}
                             width={300}
                             height={250}
@@ -268,4 +268,3 @@ export default function MainContent() {
         </div>
     )
 }
-
